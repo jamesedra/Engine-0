f018a3841ffe2299f213e958d7e30b684c218a08
jamesedra <delstherjamesedralin@gmail.com>
2025-04-15 09:26:41 -0700

    shader uniform caching

--- Engine-0/src/modules/private/shader.cpp
+++ Engine-0/src/modules/private/shader.cpp
@@ -193,40 +193,48 @@ void Shader::use()
 }
 void Shader::setBool(const std::string& name, bool value) const
 {
-	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+	glUniform1i(getUniformLocation(name), (int)value);
 }
 void Shader::setInt(const std::string& name, int value) const
 {
-	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	glUniform1i(getUniformLocation(name), value);
 }
 void Shader::setFloat(const std::string& name, float value) const
 {
-	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	glUniform1f(getUniformLocation(name), value);
 }
 
 void Shader::setVec2(const std::string& name, const glm::vec2& vec) const
 {
-	glUniform2f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y);
+	glUniform2f(getUniformLocation(name), vec.x, vec.y);
 }
 
 void Shader::setVec3(const std::string& name, float x, float y, float z) const
 {
-	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+	glUniform3f(getUniformLocation(name), x, y, z);
 }
 
 void Shader::setVec3(const std::string& name, const glm::vec3& vec) const
 {
-	glUniform3f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y, vec.z);
+	glUniform3f(getUniformLocation(name), vec.x, vec.y, vec.z);
 }
 
 void Shader::setVec4(const std::string& name, const glm::vec4& vec) const
 {
-	glUniform4f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y, vec.z, vec.w);
+	glUniform4f(getUniformLocation(name), vec.x, vec.y, vec.z, vec.w);
 }
 
 void Shader::setMat4(const std::string& name, const glm::mat4& mat) const
 {
-	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	glUniformMatrix4fv(getUniformLocation(name), 1, GL_FALSE, &mat[0][0]);
+}
+
+GLint Shader::getUniformLocation(const std::string& name) const
+{
+	if (uniformLocationCache.find(name) != uniformLocationCache.end()) return uniformLocationCache[name];
+	GLint location = glGetUniformLocation(ID, name.c_str());
+	uniformLocationCache[name] = location;
+	return location;
 }
 
 
